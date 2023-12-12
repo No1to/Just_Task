0d1030a189089c4ee2f7ce2d9452a2d271292e98
No1to <amangeldiabdibaitov@gmail.com>
2023-12-12 23:17:47 +0600

    beta 8

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,8 +9,8 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        mavenCentral()
         //https://stackoverflow.com/questions/50389211/android-studio-maven-url-https-jitpack-io-cant-download
+        mavenCentral()
         maven { url = uri("https://www.jitpack.io" ) }
     }
 }
